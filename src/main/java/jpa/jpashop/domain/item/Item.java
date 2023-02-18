b3b4f57624617b80a443338a581b77d2ac18daae
jvnlee <lhjun1028@naver.com>
2023-02-18 17:16:53 +0900

    feat: Item 클래스에 stock 증감 메서드 추가 및 stock 관련 예외 클래스 생성
    실무에서는 @Setter로 모든 필드 값에 대한 수정자를 제공하는 것보다 새로 정의한 increaseStock(), decreaseStock()처럼 비즈니스 상 필요한 로직을 메서드로 만들어서 쓰는게 좋음.
    
    decreaseStock() 실행 시 재고가 0보다 낮아지는 경우 발생시킬 NotEnoughStockException 클래스를 추가함.

--- src/main/java/jpa/jpashop/domain/item/Item.java
+++ src/main/java/jpa/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpa.jpashop.domain.item;
 
 import jpa.jpashop.domain.Category;
+import jpa.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,5 +28,17 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    public void increaseStock(int quantity) {
+        this.stock += quantity;
+    }
+
+    public void decreaseStock(int quantity) {
+        if (this.stock - quantity < 0) {
+            throw new NotEnoughStockException("재고는 0보다 낮을 수 없습니다.");
+        }
+
+        this.stock -= quantity;
+    }
 
 }

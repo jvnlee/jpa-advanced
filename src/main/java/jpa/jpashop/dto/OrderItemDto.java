ba23218c969fc933fdd42e09e4e337bbd01d6c5e
jvnlee <lhjun1028@naver.com>
2023-02-19 00:04:06 +0900

    feat: OrderService와 OrderItemDto 추가
    OrderService의 order() 메서드를 구현하는 중, 강의에서는 단순화를 위해 하나의 OrderItem만 받아서 주문하는 로직을 짰다는 것을 알게 됨. 기왕 Order의 createOrder()에서 varargs로 다수의 OrderItem을 받아오게 설계한 김에 서비스 레이어의 order() 메서드도 다수를 받을 수 있게 하는 것이 좋겠다고 판단함. 그래서 itemId와 count를 필드로 가진 OrderItemDto 클래스를 추가하고, 해당 DTO를 리스트로 받아오는 형태로 설계함.

---
+++ src/main/java/jpa/jpashop/dto/OrderItemDto.java
@@ -0,0 +1,13 @@
+package jpa.jpashop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderItemDto {
+
+    private Long itemId;
+
+    private int count;
+
+}
---
+++ src/main/java/jpa/jpashop/service/OrderService.java
@@ -0,0 +1,68 @@
+package jpa.jpashop.service;
+
+import jpa.jpashop.domain.Delivery;
+import jpa.jpashop.domain.Member;
+import jpa.jpashop.domain.Order;
+import jpa.jpashop.domain.OrderItem;
+import jpa.jpashop.domain.item.Item;
+import jpa.jpashop.dto.OrderItemDto;
+import jpa.jpashop.repository.ItemRepository;
+import jpa.jpashop.repository.MemberRepository;
+import jpa.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public Long order(Long memberId, List<OrderItemDto> orderItemList) {
+        // Member 조회
+        Member member = memberRepository.findById(memberId).orElseThrow();
+
+        // 배송 정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        // OrderItemDto를 통해 OrderItem을 생성하고 컬렉션에 모음
+        List<OrderItem> orderItems = new ArrayList<>();
+        for (OrderItemDto orderItemDto : orderItemList) {
+            Item item = itemRepository.findById(orderItemDto.getItemId()).orElseThrow();
+            OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), orderItemDto.getCount());
+
+            orderItems.add(orderItem);
+        }
+
+        OrderItem[] arr = orderItems.toArray(new OrderItem[0]);
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, arr);
+
+        /*
+        주문 영속화
+        Delivery와 OrderItem들은 CascadeType.ALL 옵션에 의해 Order가 영속화 될 때 자동적으로 함께 영속화 됨
+        그래서 따로 DeliveryRepository나 OrderItemRepository를 두고 별도로 영속화 메서드를 정의할 필요가 없음
+         */
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        Order order = orderRepository.findById(orderId).orElseThrow();
+
+        // JPA의 dirty-checking 덕분에 따로 쿼리를 작성하지 않아도 변경 사항을 반영해줌
+        order.cancelOrder();
+    }
+
+}

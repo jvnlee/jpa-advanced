5d1bbfb494d08c98ee4ebeb9756c5096e2daf48d
jvnlee <lhjun1028@naver.com>
2023-02-19 04:07:03 +0900

    test: OrderService의 searchOrder() 메서드 테스트 추가
    이름 또는 주문 상태, 그리고 둘 다를 가지고 검색하는 3가지 케이스에 대해 테스트함.

--- src/test/java/jpa/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpa/jpashop/service/OrderServiceTest.java
@@ -6,6 +6,7 @@ import jpa.jpashop.domain.Order;
 import jpa.jpashop.domain.OrderStatus;
 import jpa.jpashop.domain.item.Book;
 import jpa.jpashop.dto.OrderItemDto;
+import jpa.jpashop.dto.OrderSearchDto;
 import jpa.jpashop.exception.NotEnoughStockException;
 import jpa.jpashop.repository.MemberRepository;
 import jpa.jpashop.repository.OrderRepository;
@@ -40,7 +41,7 @@ class OrderServiceTest {
     @Test
     @DisplayName("주문 성공")
     void order() {
-        Member member = createSampleMember();
+        Member member = createSampleMember("Andy");
         Book book1 = createSampleBook("BookA", 10000, 10);
         Book book2 = createSampleBook("BookB", 12000, 20);
         List<OrderItemDto> dtoList = createOrderItemDtoList(book1, book2, 5, 10);
@@ -58,7 +59,7 @@ class OrderServiceTest {
     @Test
     @DisplayName("주문 실패 - 재고 부족")
     void order_outOfStock() {
-        Member member = createSampleMember();
+        Member member = createSampleMember("Andy");
         Book book1 = createSampleBook("BookA", 10000, 10);
         Book book2 = createSampleBook("BookB", 12000, 20);
         List<OrderItemDto> dtoList = createOrderItemDtoList(book1, book2, 5, 1000);
@@ -70,7 +71,7 @@ class OrderServiceTest {
     @Test
     @DisplayName("주문 취소")
     void cancelOrder() {
-        Member member = createSampleMember();
+        Member member = createSampleMember("Andy");
         Book book1 = createSampleBook("BookA", 10000, 10);
         Book book2 = createSampleBook("BookB", 12000, 20);
         List<OrderItemDto> dtoList = createOrderItemDtoList(book1, book2, 5, 10);
@@ -84,9 +85,76 @@ class OrderServiceTest {
         assertThat(book2.getStock()).isEqualTo(20);
     }
 
-    private Member createSampleMember() {
+    @Test
+    @DisplayName("주문 내역 검색 - 이름")
+    void searchOrder_memberName() {
+        Member member1 = createSampleMember("Andy");
+        Member member2 = createSampleMember("Lee");
+        Book book1 = createSampleBook("BookA", 10000, 10);
+        Book book2 = createSampleBook("BookB", 12000, 20);
+        List<OrderItemDto> dtoList1 = createOrderItemDtoList(book1, book2, 1, 1);
+        List<OrderItemDto> dtoList2 = createOrderItemDtoList(book1, book2, 2, 2);
+
+        orderService.order(member1.getId(), dtoList1);
+        orderService.order(member2.getId(), dtoList2);
+
+        OrderSearchDto searchCond = new OrderSearchDto();
+        searchCond.setMemberName("Lee");
+
+        List<Order> result = orderService.searchOrder(searchCond);
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(result.get(0).getMember().getName()).isEqualTo("Lee");
+        assertThat(result.get(0).getStatus()).isEqualTo(OrderStatus.ORDER);
+        assertThat(result.get(0).getTotalPrice()).isEqualTo(44000);
+    }
+
+    @Test
+    @DisplayName("주문 내역 검색 - 주문상태")
+    void searchOrder_orderStatus() {
+        Member member1 = createSampleMember("Andy");
+        Member member2 = createSampleMember("Lee");
+        Book book1 = createSampleBook("BookA", 10000, 10);
+        Book book2 = createSampleBook("BookB", 12000, 20);
+        List<OrderItemDto> dtoList1 = createOrderItemDtoList(book1, book2, 1, 1);
+        List<OrderItemDto> dtoList2 = createOrderItemDtoList(book1, book2, 2, 2);
+
+        orderService.order(member1.getId(), dtoList1);
+        orderService.order(member2.getId(), dtoList2);
+
+        OrderSearchDto searchCond = new OrderSearchDto();
+        searchCond.setOrderStatus(OrderStatus.CANCEL);
+
+        List<Order> result = orderService.searchOrder(searchCond);
+        assertThat(result.size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("주문 내역 검색 - 이름과 주문상태")
+    void searchOrder() {
+        Member member1 = createSampleMember("Andy");
+        Member member2 = createSampleMember("Lee");
+        Book book1 = createSampleBook("BookA", 10000, 10);
+        Book book2 = createSampleBook("BookB", 12000, 20);
+        List<OrderItemDto> dtoList1 = createOrderItemDtoList(book1, book2, 1, 1);
+        List<OrderItemDto> dtoList2 = createOrderItemDtoList(book1, book2, 2, 2);
+
+        orderService.order(member1.getId(), dtoList1);
+        orderService.order(member2.getId(), dtoList2);
+
+        OrderSearchDto searchCond = new OrderSearchDto();
+        searchCond.setMemberName("Andy");
+        searchCond.setOrderStatus(OrderStatus.ORDER);
+
+        List<Order> result = orderService.searchOrder(searchCond);
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(result.get(0).getMember().getName()).isEqualTo("Andy");
+        assertThat(result.get(0).getStatus()).isEqualTo(OrderStatus.ORDER);
+        assertThat(result.get(0).getTotalPrice()).isEqualTo(22000);
+    }
+
+    private Member createSampleMember(String name) {
         Member member = new Member();
-        member.setName("Andy");
+        member.setName(name);
         member.setAddress(new Address("Seoul", "Teheran-ro", "12345"));
         em.persist(member);
         return member;

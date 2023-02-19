930ffaebcd711d4aa49ff86e508d857180c8fbbe
jvnlee <lhjun1028@naver.com>
2023-02-19 23:52:26 +0900

    refactor: MemberService의 메서드가 Member 대신 MemberDto를 반환하도록 변경

--- src/main/java/jpa/jpashop/service/MemberService.java
+++ src/main/java/jpa/jpashop/service/MemberService.java
@@ -1,6 +1,7 @@
 package jpa.jpashop.service;
 
 import jpa.jpashop.domain.Member;
+import jpa.jpashop.dto.MemberDto;
 import jpa.jpashop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -16,21 +18,27 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     @Transactional
-    public Long join(Member member) {
+    public Long join(MemberDto memberDto) {
         // 동시에 접근한 같은 이름의 멤버가 검증을 통과하는 경우도 고려해서 name 컬럼 자체에 unique 제약 조건을 넣는 것이 좋음
-        validateDuplicateName(member.getName());
+        validateDuplicateName(memberDto.getName());
+        Member member = new Member();
+        member.update(memberDto.getName(), memberDto.getAddress());
         memberRepository.save(member);
         return member.getId();
     }
 
     @Transactional(readOnly = true) // 조회 메서드는 JPA 성능 최적화를 위해 readOnly 옵션 true로 설정
-    public Member findMember(Long id) {
-        return memberRepository.findById(id).orElseThrow();
+    public MemberDto findMember(Long id) {
+        Member member = memberRepository.findById(id).orElseThrow();
+        return new MemberDto(member.getName(), member.getAddress());
     }
 
     @Transactional(readOnly = true)
-    public List<Member> findMembers() {
-        return memberRepository.findAll();
+    public List<MemberDto> findMembers() {
+        List<Member> memberList = memberRepository.findAll();
+        return memberList.stream()
+                .map(m -> new MemberDto(m.getName(), m.getAddress()))
+                .collect(Collectors.toList());
     }
 
     private void validateDuplicateName(String name) {
